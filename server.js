0a7624d1be02dacd6a6e4861c19b8808cf8906c1
leandromarten1 <leandromarten1@gmail.com>
2021-04-15 09:29:29 -0300

    Inittial Commit - Basic Server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+const PORT = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${PORT}!`));
