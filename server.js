d5983946b77e08f98cc38b973787881393f1e803
leandromarten1 <leandromarten1@gmail.com>
2021-04-15 09:51:06 -0300

    Sequelize - Change config.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv/config');
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 const PORT = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
